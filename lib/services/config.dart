73863527d6c1c55610edba2e22e639ece5adce46
Naveenkumarsundaresh <138775980+Naveenkumarsundaresh@users.noreply.github.com>
2024-04-05 23:54:16 +0530

    Add files via upload

---
+++ lib/services/config.dart
@@ -0,0 +1,12 @@
+class Config {
+  static const String apiUrl = "jobhubrest-production-004a.up.railway.app";
+  static const String loginUrl = "/api/login";
+  static const String signupUrl = "/api/register";
+  static const String jobs = "/api/jobs";
+  static const String search = "/api/jobs/search";
+  static const String job = "/api/jobs";
+  static const String profileUrl = "/api/users";
+  static const String bookmarkUrl = "/api/bookmarks";
+  static const String chatsUrl = "/api/chats";
+  static const String messagingUrl = "/api/messages";
+}
---
+++ lib/services/helpers/jobs_helper.dart
@@ -0,0 +1,89 @@
+import 'package:http/http.dart' as https;
+import 'package:jobhub/models/response/jobs/get_job.dart';
+import 'package:jobhub/models/response/jobs/jobs_response.dart';
+import 'package:jobhub/services/config.dart';
+
+class JobsHelper {
+  static var client = https.Client();
+
+  static Future<List<JobsResponse>> getJobs() async {
+    Map<String, String> requestHeaders = {
+      'Content-Type': 'application/json',
+    };
+
+    var url = Uri.https(Config.apiUrl, Config.jobs);
+    var response = await client.get(
+      url,
+      headers: requestHeaders,
+    );
+
+    if (response.statusCode == 200) {
+      var jobsList = jobsResponseFromJson(response.body);
+      return jobsList;
+    } else {
+      throw Exception("Failed to get the jobs");
+    }
+  }
+
+// get job
+  static Future<GetJobRes> getJob(String jobId) async {
+    Map<String, String> requestHeaders = {
+      'Content-Type': 'application/json',
+    };
+
+    var url = Uri.https(Config.apiUrl, "${Config.jobs}/$jobId");
+    var response = await client.get(
+      url,
+      headers: requestHeaders,
+    );
+
+    if (response.statusCode == 200) {
+      var job = getJobResFromJson(response.body);
+      return job;
+    } else {
+      throw Exception("Failed to get a job");
+    }
+  }
+
+  static Future<JobsResponse> getRecent() async {
+    Map<String, String> requestHeaders = {
+      'Content-Type': 'application/json',
+    };
+
+    var url = Uri.https(Config.apiUrl, Config.jobs);
+    var response = await client.get(
+      url,
+      headers: requestHeaders,
+    );
+
+    if (response.statusCode == 200) {
+      var jobsList = jobsResponseFromJson(response.body);
+
+      var recent = jobsList.first;
+      return recent;
+    } else {
+      throw Exception("Failed to get the jobs");
+    }
+  }
+
+
+//SEARCH 
+ static Future<List<JobsResponse>> searchJobs(String searchQeury) async {
+    Map<String, String> requestHeaders = {
+      'Content-Type': 'application/json',
+    };
+
+    var url = Uri.https(Config.apiUrl, "${Config.search}/$searchQeury");
+    var response = await client.get(
+      url,
+      headers: requestHeaders,
+    );
+
+    if (response.statusCode == 200) {
+      var jobsList = jobsResponseFromJson(response.body);
+      return jobsList;
+    } else {
+      throw Exception("Failed to get the jobs");
+    }
+  }
+}
